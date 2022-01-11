127997cb7bafa6758f387bbe7b0eba66e87c8910
thuantieu <tieuvinhthuan.fi@gmail.com>
2022-01-11 20:05:08 +0200

    Setting up roject

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-app.use(cors({optionsSuccessStatus: 200}));
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.get("/", (req, res) => {
+  res.send("Hello World! Hello!");
+  
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
